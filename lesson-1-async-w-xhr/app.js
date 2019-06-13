dab5bdf8e92b7dc8803b4b686682836c8148e8dd
Tatiana Zaslavskaia <44182488+TatianaWeb@users.noreply.github.com>
2019-06-13 17:31:29 +0400

    add start script from Udacity course

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,7 +1,15 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
+    //let searchedForText;
+    function addImage(){}
+    const searchedForText = 'hippos';
+    const unsplashRequest = new XMLHttpRequest();
+
+    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    unsplashRequest.onload = addImage;
+
+    unsplashRequest.send()
     const responseContainer = document.querySelector('#response-container');
 
     form.addEventListener('submit', function (e) {
